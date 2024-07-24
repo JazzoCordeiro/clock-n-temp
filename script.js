291743648547002bcb8104c4ff4560426c061b38
jazzocordeiro <cordeirorosadigital@gmail.com>
2024-07-23 22:38:52 -0300

    primeiro commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function load() {
     var img = window.document.getElementById('image');
     var data = new Date();
     var day = data.getDate();
-    var month = data.getMonth() +1;
+    var month = data.getMonth() + 1;
     var year = data.getFullYear();
     var hour = data.getHours();
     var minutes = data.getMinutes();
@@ -12,20 +12,19 @@ function load() {
 
     if (hour >= 5 && hour < 12) {
         greeting = 'Good Morning!';
-         img.src = 'morning.png'
-        document.body.style.background = '#69a5d7'
+        img.src = 'morning.png';
+        document.body.style.background = '#69a5d7';
     } else if (hour >= 12 && hour < 18) {
         greeting = 'Good Afternoon!';
-        img.src = 'noon.png'
-        document.body.style.background = '#faf4c2'
+        img.src = 'noon.png';
+        document.body.style.background = '#faf4c2';
     } else {
         greeting = 'Good Night!';
-        img.src = 'night.png'
-        document.body.style.background = '#022022'
+        img.src = 'night.png';
+        document.body.style.background = '#022022';
     }
 
     msg.innerHTML = `${day}/${month}/${year}, <br> It's ${hour} hours and ${minutes} minutes and ${seconds} seconds. ${greeting}`;
 }
 
 setInterval(load, 1000);
-
--- script2.js
+++ script2.js
@@ -1,28 +1,22 @@
 const key = "d1500ecb1edc872f50a80ddf6171ac70";
 
-function colocarDadosNaTela(dados){
-    console.log(dados)
+function colocarDadosNaTela(dados) {
+    console.log(dados);
     window.document.querySelector(".place").innerHTML = "Weather in " + dados.name;
     window.document.querySelector(".temp").innerHTML = "Temperature now " + Math.floor(dados.main.temp) + "°C";
     window.document.querySelector(".rainy").innerHTML = dados.weather[0].description;
-    window.document.querySelector(".maxTemp").innerHTML = "Max. Temp. " + Math.floor(dados.main.temp_max) + "°C";
-    window.document.querySelector(".minTemp").innerHTML = "Min. Temp. " + Math.floor(dados.main.temp_min) + "°C";
+    window.document.querySelector(".maxTemp").innerHTML = "Max Temp " + Math.floor(dados.main.temp_max) + "°C";
+    window.document.querySelector(".minTemp").innerHTML = "Min Temp " + Math.floor(dados.main.temp_min) + "°C";
     window.document.querySelector(".humidity").innerHTML = dados.main.humidity + "% humidity";
     window.document.querySelector(".weatherIcon").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`;
 }
 
-
 async function buscarCidade(cidade) {
-    const dados= await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&units=metric`).then(resposta => resposta.json());
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&units=metric`).then(resposta => resposta.json());
     colocarDadosNaTela(dados);
 }
 
-
-function cliqueiNoBotao(){
-    const cidade = window.document.querySelector(".cidade").value
-    buscarCidade(cidade)
+function cliqueiNoBotao() {
+    const cidade = window.document.querySelector(".cidade").value;
+    buscarCidade(cidade);
 }
-
-
-
-    
